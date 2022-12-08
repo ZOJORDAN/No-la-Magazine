a1f825b1b264b9ce8d5db3453e477bc5da1f40a9
Ange <angeericstephane.kouakou@ynov.com>
2022-12-08 03:45:35 +0100

    making it more modular

--- script/toggle.js
+++ script/toggle.js
@@ -1,7 +1,9 @@
 function toggle(btnID){
   var theButton = document.getElementById(btnID);
+  var element = document.body;
   if (theButton.getAttribute("aria-pressed") == "false"){
     theButton.setAttribute("aria-pressed", "true");
+    element.classList.toggle("dark-mode");
   }
   else{
     theButton.setAttribute("aria-pressed", "false");
